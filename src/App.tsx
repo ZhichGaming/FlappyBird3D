b0666941352faa86c4e4fab1f842bb9fcd022731
ZhichGaming <nick.zhicheng@gmail.com>
2024-03-20 09:04:22 -0400

    feat: unimplemented laser system

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import "./App.css";
 import Game from "./Game";
 import Game2D from "./Game2D";
+import Laser from "./Laser";
 
 export let game: Game;
 export let game2d: Game2D;
@@ -10,8 +11,11 @@ function App() {
   useEffect(() => {
     // game = new Game();
     // game.start();
-    game2d = new Game2D();
-    game2d.start();
+    // game2d = new Game2D();
+    // game2d.start();
+    const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+    const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+    let laser = new Laser(canvas, ctx);
   }, []);
 
   return (
