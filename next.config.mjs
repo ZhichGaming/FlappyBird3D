b07fda28dbe086dbb8976638b898c2d615a3d4b5
ZhichGaming <nick.zhicheng@gmail.com>
2024-04-13 01:23:07 -0400

    fix: asset path

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",  // <=== enables static exports
+    basePath: "/dimensional-bird",
 };
 
 export default nextConfig;
